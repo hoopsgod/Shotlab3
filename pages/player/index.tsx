abdd155905f142b5adcf006778b392fc1da37396
hoopsgod <jcharno42@gmail.com>
2026-03-18 18:31:06 -0400

    Fix player demo entrypoint mount

--- pages/player/index.tsx
+++ pages/player/index.tsx
@@ -1,10 +1,13 @@
+import ReactDOM from 'react-dom/client';
 import PlayerDashboardLayout from '../../layouts/PlayerDashboardLayout';
 import HomePage from '../../features/player/HomePage';
 
-export default function PlayerHome() {
+function PlayerHome() {
   return (
     <PlayerDashboardLayout>
       <HomePage />
     </PlayerDashboardLayout>
   );
 }
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<PlayerHome />);
