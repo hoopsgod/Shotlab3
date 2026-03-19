cdb077ee7b44ab3b3fe3f5c444f5a8d218e8c704
hoopsgod <jcharno42@gmail.com>
2026-03-19 18:04:54 -0400

    Rollback repo to 3ea1cf4 snapshot

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
-    assetsInlineLimit: 1048576,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        playerDemo: resolve(__dirname, 'player/index.html'),
-      },
-    },
+    assetsInlineLimit: 1048576
   }
 })
