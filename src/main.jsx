6cea62cf8338f3d8a003ded5607ef60961a77250
hoopsgod <jcharno42@gmail.com>
2026-04-11 22:41:10 -0400

    Revert recent help-system changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { demoBootstrap } from './lib/demoBootstrap.ts'
-import './help/help.css'
 
 demoBootstrap()
 
