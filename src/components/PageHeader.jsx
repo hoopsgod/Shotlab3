468632a2fe553c7745fd742787326fe8074125fb
hoopsgod <jcharno42@gmail.com>
2026-05-10 13:45:25 -0400

    Adjust PageHeader icon polish for Cloudflare-safe CSS parsing

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -38,6 +38,7 @@ export default function PageHeader({
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
+              boxShadow: "inset 0 0 0 1px rgba(255,255,255,0.06)",
             }}
           >
             {icon}
