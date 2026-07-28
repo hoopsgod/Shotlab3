3a2c1fc55973e358844cd8e1b7d40759f38de8bf
hoopsgod <jcharno42@gmail.com>
2026-07-28 09:03:20 -0400

    build: enable player workspace integration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import playerWorkspacesPhase2Plugin from './scripts/playerWorkspacesPhase2VitePlugin.mjs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [playerWorkspacesPhase2Plugin(), react()],
   base: './',
   build: {
     outDir: 'dist',
