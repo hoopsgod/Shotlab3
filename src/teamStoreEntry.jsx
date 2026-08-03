bad9e5aa2d7c82eeffb54c9bab3dce42e3fbc0d2
hoopsgod <jcharno42@gmail.com>
2026-08-03 13:22:07 -0400

    Mount Team Store feedback system

--- src/teamStoreEntry.jsx
+++ src/teamStoreEntry.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppFeedbackLayer from "./components/AppFeedbackLayer.jsx";
+import TeamStoreFeedbackBridge from "./components/TeamStoreFeedbackBridge.jsx";
 import TeamStorePortal from "./components/TeamStorePortal.jsx";
 import "./components/TeamStoreIndustrial.css";
 
@@ -7,6 +9,8 @@ const mount = document.getElementById("team-store-root");
 if (mount) {
   ReactDOM.createRoot(mount).render(
     <React.StrictMode>
+      <AppFeedbackLayer />
+      <TeamStoreFeedbackBridge />
       <TeamStorePortal />
     </React.StrictMode>,
   );
