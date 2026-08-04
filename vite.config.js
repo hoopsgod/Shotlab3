48c6c0178c34d86c9596fbe04a1ec6e683dcf734
hoopsgod <jcharno42@gmail.com>
2026-08-04 12:24:01 -0400

    Establish stable vendor chunks for the performance phase

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const normalizeModuleId = (id = '') => String(id).replaceAll('\\', '/')
+
+function stableVendorChunk(id) {
+  const moduleId = normalizeModuleId(id)
+
+  if (
+    moduleId.includes('/node_modules/react/')
+    || moduleId.includes('/node_modules/react-dom/')
+    || moduleId.includes('/node_modules/scheduler/')
+  ) {
+    return 'react-vendor'
+  }
+
+  if (moduleId.includes('/vendor/recharts/')) {
+    return 'charts-vendor'
+  }
+
+  return undefined
+}
+
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
-    assetsInlineLimit: 1048576
-  }
+    assetsInlineLimit: 1048576,
+    reportCompressedSize: true,
+    chunkSizeWarningLimit: 850,
+    rollupOptions: {
+      output: {
+        manualChunks: stableVendorChunk,
+      },
+    },
+  },
 })
