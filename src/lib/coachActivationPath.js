98309b13c50db90a02a4b3822cc68e1e2970a5c7
hoopsgod <jcharno42@gmail.com>
2026-09-21 17:40:36 -0400

    Remove accidental Phase 7A enhancer coupling

--- src/lib/coachActivationPath.js
+++ src/lib/coachActivationPath.js
@@ -8,7 +8,6 @@ import { installCoachAssignmentInterventionEnhancer } from "./coachAssignmentInt
 import { installCoachInterventionOutcomeEnhancer } from "./coachInterventionOutcomeEnhancer.js";
 import { installCoachAssignmentOutcomeEnhancer } from "./coachAssignmentOutcomeEnhancer.js";
 import { installCoachHomeHierarchyEnhancer } from "./coachHomeHierarchyEnhancer.js";
-import { installCoachFollowUpEnhancer } from "./coachFollowUpEnhancer.js";
 import { installCoachQuickAssignEnhancer } from "./coachQuickAssignEnhancer.js";
 import { installCoachResponseLoopEnhancer } from "./coachResponseLoopEnhancer.js";
 import { installHomeExperienceHierarchy } from "./homeExperienceHierarchy.js";
@@ -27,7 +26,6 @@ installCoachAssignmentActionPromptsEnhancer();
 installCoachAssignmentInterventionEnhancer();
 installCoachInterventionOutcomeEnhancer();
 installCoachHomeHierarchyEnhancer();
-installCoachFollowUpEnhancer();
 installCoachResponseLoopEnhancer();
 installVisualSystemReboot();
 installVisualSystemRebootReleaseFixes();
