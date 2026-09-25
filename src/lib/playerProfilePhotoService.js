bf985fec16b3016508f6598ce90da16a1e9e843b
hoopsgod <jcharno42@gmail.com>
2026-09-25 15:56:02 -0400

    Trim profile photo upload service

--- src/lib/playerProfilePhotoService.js
+++ src/lib/playerProfilePhotoService.js
@@ -3,10 +3,9 @@ import { isDemoAccount } from "./demoMode.js";
 
 export async function savePlayerProfilePhoto(id, file) {
   if (isDemoAccount(id)) return URL.createObjectURL(file);
-  const body = new FormData();
+  const body = new FormData;
   body.append("file", file);
-  const response = await fetch("/v1/player-photo", { method: "POST", headers: buildApiIdentityHeaders({ requester: id }), body });
-  const data = await response.json().catch(() => ({}));
+  const data = await (await fetch("/v1/player-photo", { method: "POST", headers: buildApiIdentityHeaders({ requester: id }), body })).json().catch(() => ({}));
   if (!data.photo_url) throw Error();
   return data.photo_url;
 }
